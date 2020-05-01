efc54b3518954fb0140c66bd2ac72c77dfc61ef1
Kevin Li <kevinli4321@gmail.com>
2020-05-01 09:59:50 -0400

    Tranpose columns to rows in dataset, obtain data as per spec on ddoc

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from utl import getdata
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html");
+    dict = getdata.getData()
+    return render_template("index.html", dict = dict.items());
 
 if __name__ == "__main__":
     app.debug = True
---
+++ app/utl/getdata.py
@@ -0,0 +1,30 @@
+import csv
+
+# Run once every time __init__.py runs (through the import statement)
+# =============================================================================
+CSV_FILE = "static/epidemic-deaths-by-day.csv"
+
+epidemic_names = ['cholera-hispaniola-2010', 'covid-19', 'ebola-wafrica-2014',
+                  'swine-world-2009', 'sars', 'measles-2019', 'measles-2011',
+                  'cholera-zimbabwe-2008', 'cholera-yemen-2016', 'ebola-drcuganda-2018',
+                  'swine-india-2015', 'meningitis', 'mers']
+
+epidemics_dict = {}
+epidemic_idx = 0
+
+file = open(CSV_FILE)
+data = csv.reader(file)
+for row in data:
+    arr = []
+    for day in row[1:]: #first column is the disease name not a day's stats
+        arr.append(day)
+
+    epidemics_dict[epidemic_names[epidemic_idx]] = arr
+    epidemic_idx += 1
+
+file.close()
+
+# =============================================================================
+
+def getData():
+    return epidemics_dict
