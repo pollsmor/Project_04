ffa9b5ae6be7fa46d936b634bb9e70d660d759d5
Derek Leung <dleung00@stuy.edu>
2020-05-01 18:07:04 -0400

    added axes labels

--- app/static/d3.js
+++ app/static/d3.js
@@ -12,20 +12,30 @@ var render = function(e){
     .attr("height",height);
   var xAxisScale = d3.scaleLinear()
     .domain([0,10])
-    .range([0,width-40]);
+    .range([0,width-50]);
   var xAxis = d3.axisBottom()
     .scale(xAxisScale);
   svg.append("g")
-    .attr("transform", "translate(25," + (height-20) + ")")
+    .attr("transform", "translate(35," + (height-40) + ")")
     .call(xAxis);
   var yAxisScale = d3.scaleLinear()
     .domain([0,10])
-    .range([height-30,0]);
+    .range([height-50,0]);
   var yAxis = d3.axisLeft()
     .scale(yAxisScale);
   svg.append("g")
-    .attr("transform", "translate(25, 10)")
+    .attr("transform", "translate(35, 10)")
     .call(yAxis);
+  svg.append("text")
+    .style("text-anchor","middle")
+    .attr("transform", "translate(" + (width/2) + "," + (height-5) + ")")
+    .text("Days since beginning of outbreak");
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y",  15)
+        .attr("x",-(height / 2))
+        .style("text-anchor", "middle")
+        .text("Deaths per day");
 };
 
 var transition = function(e){
