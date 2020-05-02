fbaa07a2adc1afde7d601dfa3235af0b0f62f6f7
Kevin Li <kevinli4321@gmail.com>
2020-05-02 15:57:38 -0400

    More proper scaling with default 300-day dataset

--- app/static/d3.js
+++ app/static/d3.js
@@ -1,40 +1,42 @@
-var svg;
-var x,y;
-var width = 500;
+var svg = d3.select("svg");
+var x, y;
+var width = 750;
 var height = 500;
+
+var days = 300;
 
 $("[data-toggle=tooltip]").tooltip(); //enable Bootstrap tooltips
 
 var render = function() {
-  var svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height",height);
+  svg.attr("width", width).attr("height", height);
+
   var xAxisScale = d3.scaleLinear()
-    .domain([0,10])
-    .range([0,width-50]);
+    .domain([1, days])
+    .range([0, width - 50]);
   var xAxis = d3.axisBottom()
     .scale(xAxisScale);
   svg.append("g")
-    .attr("transform", "translate(35," + (height-40) + ")")
+      .attr("transform", "translate(80," + (height - 40) + ")")
     .call(xAxis);
+  svg.append("text")
+      .style("text-anchor","middle")
+      .attr("transform", "translate(" + (width / 2) + "," + (height - 5) + ")")
+    .text("Days since beginning of outbreak");
+
   var yAxisScale = d3.scaleLinear()
-    .domain([0,10])
-    .range([height-50,0]);
+    .domain([0, 200000])
+    .range([height - 50, 0]);
   var yAxis = d3.axisLeft()
     .scale(yAxisScale);
   svg.append("g")
-    .attr("transform", "translate(35, 10)")
+      .attr("transform", "translate(80, 10)")
     .call(yAxis);
   svg.append("text")
-    .style("text-anchor","middle")
-    .attr("transform", "translate(" + (width/2) + "," + (height-5) + ")")
-    .text("Days since beginning of outbreak");
-    svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y",  15)
-        .attr("x",-(height / 2))
-        .style("text-anchor", "middle")
-        .text("Deaths per day");
+      .attr("transform", "rotate(-90)")
+      .attr("y",  15)
+      .attr("x",-(height / 2))
+      .style("text-anchor", "middle")
+      .text("Deaths per day");
 };
 
 render();
