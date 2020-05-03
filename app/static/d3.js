230f14f9fb4394ae448f38539d2afcf34302904a
jjshawshaw <jshaw00@stuy.edu>
2020-05-03 19:19:07 -0400

    added line colors

--- app/static/d3.js
+++ app/static/d3.js
@@ -21,6 +21,20 @@ var rendered = {'cholera-hispaniola-2010': false,
                 'meningitis' : false,
                 'mers' : false};
 
+var colors = {'cholera-hispaniola-2010': "BlueViolet",
+                'covid-19': "DodgerBlue",
+                'ebola-wafrica-2014' : "DarkGreen",
+                'swine-world-2009' : "DarkSlateGray",
+                'sars' : "Gold",
+                'measles-2019' : "IndianRed",
+                'measles-2011' : "HotPink",
+                'cholera-zimbabwe-2008' : "SandyBrown",
+                'cholera-yemen-2016' : "Silver",
+                'ebola-drcuganda-2018' : "GoldenRod",
+                'swine-india-2015' : "SteelBlue",
+                'meningitis' : "SpringGreen",
+                'mers' : "Thistle"};
+
 var render = function() {
 
   svg.attr("width", width).attr("height", height);
@@ -62,7 +76,7 @@ var render = function() {
         svg.append("path")
           .attr("transform", "translate(80, 10)")
           .attr("fill", "none")
-          .attr("stroke", "DodgerBlue")
+          .attr("stroke", colors[name])
           .attr("stroke-width", 1.5)
           .attr("d", line(data[name]));
       }
